690b5617631781672b58993a642a9b873b9868f6
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-09-24 19:50:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// const btn =document.getElementById("toggle")
+// const theme=document.querySelector("#theme-link");
+// btn.addEventListener("click",function (){
+//     if(theme.getAttribute("href")=="https://cdn.jsdelivr.net/npm/dark-mode-switch@1.0.0/dark-mode-switch.min.js"){
+// theme.href="https://cdn.jsdelivr.net/npm/dark-mode-switch@1.0.0/dark-mode-switch.min.js";
+
+//     }else {
+//     theme.href="https://cdn.jsdelivr.net/npm/dark-mode-switch@1.0.0/dark-mode-switch.min.js";
+
+
+//     }
+// });\\
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+ 
